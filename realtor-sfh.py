ccfe9a79dc650c2939110d2d2f2ed37fabd00282
nronzel <86695181+nronzel@users.noreply.github.com>
2023-05-13 20:28:46 -0400

    add: ratios, fixed URL
    large commit, woops

--- realtor-sfh.py
+++ realtor-sfh.py
@@ -3,13 +3,14 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import time
 import logging
+import house_to_yard as hy
 
 logging.basicConfig(level=logging.INFO)
 
 
 LOCATION = input("Location (e.g. Miami_FL, 90210): ").replace(" ", "")
 BASE_URL = "https://www.realtor.com/realestateandhomes-search/"
-STUB = f"{LOCATION}/type-single-family-home/price-100000-na/age-3/sby-6/"
+STUB = f"{LOCATION}/beds-1/baths-1/type-single-family-home/price-100000-na/age-3/sby-6/"
 URL = BASE_URL + STUB
 PAGES = 5
 AGENT = "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/113.0"
@@ -26,25 +27,43 @@ def getSoup(URL):
         return None
 
 
-def get_home_data(house):
+def getHomeData(house):
     data = house.find("div", class_="property-wrap")
     price = data.span.text.replace("$", "")
     beds = data.find("li", attrs={"data-label": "pc-meta-beds"}).span.text
     baths = data.find("li", attrs={"data-label": "pc-meta-baths"}).span.text
-    sqft = data.find("li", attrs={"data-label": "pc-meta-sqft"}).span.text
+    try:
+        sqft = data.find("li", attrs={"data-label": "pc-meta-sqft"}).span.text.replace(
+            ",", ""
+        )
+    except AttributeError:
+        sqft = "-"
     try:
         lotsize = data.find("li", attrs={"data-label": "pc-meta-sqftlot"}).text
+        size = hy.parse_lot_size(lotsize)
     except AttributeError:
         lotsize = "-"
+        size = "-"
     address = data.find("div", attrs={"data-label": "pc-address"}).text
     link = house.find("div", class_="photo-wrap").a["href"]
+
+    hty_ratio = "-"
+    house_ratio = "-"
+
+    if size != "-" and sqft != "-":
+        hty_ratio = round(int(size) / int(sqft), 2)
+        house_ratio = round(int(sqft) / int(size), 2)
 
     return {
         "PRICE $": price,
         "BEDS": beds,
         "BATHS": baths.replace("+", ""),
-        "SQFT": sqft,
-        "LOTSIZE": lotsize,
+        "SQFT": int(sqft.replace(",", "")),
+        "LOTSIZE": hy.split_lot_size(lotsize)[0],
+        "LOTUNIT": hy.split_lot_size(lotsize)[1],
+        "SIZE": size,
+        "HtY": hty_ratio or "-",
+        "HOUSE RATIO": house_ratio or "-",
         "ADDRESS": address,
         "LINK": f"https://www.realtor.com{link}",
     }
@@ -55,26 +74,31 @@ def parseSoup(URL):
     if soup is None:  # If there was an error getting the page
         return []
     homes = []
-    for house in soup.find_all("li", attrs={"data-testid": "result-card"}):
-        homes.append(get_home_data(house))
+    house_elements = soup.find_all("li", attrs={"data-testid": "result-card"})
+    if not house_elements:  # If there are no house elements, stop scraping
+        return None
+    for house in house_elements:
+        homes.append(getHomeData(house))
     return homes
 
 
 def scrapeIt():
     all_homes = []
-    for i in range(1, PAGES):
+    for i in range(1, PAGES + 1):
         URL = BASE_URL + STUB + f"pg-{i}"
-        all_homes += parseSoup(URL)
+        new_homes = parseSoup(URL)
+        if new_homes is None:  # If there are no new homes, stop scraping
+            break
+        all_homes += new_homes
         logging.info(f"Scraped: {URL}")
-        time.sleep(2)
-
+        time.sleep(3)
     return all_homes
 
 
 def sendIt():
     homes = scrapeIt()
     df = pd.DataFrame(homes)
-    df.to_csv(f"{LOCATION}.csv", index=False)
+    df.to_csv(f"scans/{LOCATION}.csv", index=False)
     logging.info(df)
 
 
