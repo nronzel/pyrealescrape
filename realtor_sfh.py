1c3d132b6589d18cf114f834986efd3717ffeda9
nronzel <86695181+nronzel@users.noreply.github.com>
2023-05-14 15:11:35 -0400

    rename + feature: address parsed

---
+++ realtor_sfh.py
@@ -0,0 +1,141 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+import time
+import logging
+import house_to_yard as hy
+
+logging.basicConfig(level=logging.INFO)
+
+HOME_TYPE = "type-single-family-home"
+PAGES = 5
+MIN_PRICE = "price-100000-na"
+
+LOCATION = input("Location (e.g. Miami_FL, 90210): ").replace(" ", "")
+BASE_URL = "https://www.realtor.com/realestateandhomes-search/"
+OPTIONS = (
+    f"{LOCATION}/beds-1/baths-1/{HOME_TYPE}/{MIN_PRICE}/age-3+/pnd-hide/55p-hide/sby-6/"
+)
+
+URL = BASE_URL + OPTIONS
+AGENT = "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/113.0"
+
+
+def getSoup(URL):
+    headers = {"user-agent": AGENT}
+    try:
+        page = requests.get(URL, headers=headers)
+        if page.status_code != 200:
+            logging.error(f"Request returned status code {page.status_code}")
+            raise requests.exceptions.HTTPError(
+                f"HTTPError: Received status code {page.status_code}"
+            )
+        soup = BeautifulSoup(page.content, "html.parser")
+        return soup
+    except requests.exceptions.RequestException as e:
+        logging.error(f"RequestException: {e}")
+        return None
+
+
+def extract_text(data, attr, default="-"):
+    element = data.find("li", attrs={"data-label": attr})
+    return element.span.text if element else default
+
+
+def getHomeData(house):
+    data = house.find("div", class_="property-wrap")
+    price = data.span.text.replace("$", "")
+    beds = extract_text(data, "pc-meta-beds")
+    baths = extract_text(data, "pc-meta-baths")
+
+    try:
+        sqft = extract_text(data, "pc-meta-sqft").replace(",", "")
+    except AttributeError:
+        sqft = "-"
+
+    try:
+        lotsize = data.find("li", attrs={"data-label": "pc-meta-sqftlot"}).text
+        size = hy.parse_lot_size(lotsize) if lotsize != "-" else "-"
+    except AttributeError:
+        lotsize = "-"
+        size = "-"
+
+    try:
+        address = data.find("div", attrs={"data-label": "pc-address"}).text
+    except AttributeError:
+        address = "-"
+
+    link = house.find("div", class_="photo-wrap").a["href"]
+
+    hty_ratio = "-"
+    house_ratio = "-"
+
+    if size != "-" and sqft != "-" and size != 0:
+        hty_ratio = round(int(size) / int(sqft), 2)
+        house_ratio = round(int(sqft) / int(size), 2)
+
+    return {
+        "PRICE $": price,
+        "BEDS": beds,
+        "BATHS": baths.replace("+", ""),
+        "SQFT": int(sqft.replace(",", "")) if sqft != "-" else "-",
+        "LOTSIZE": hy.split_lot_size(lotsize)[0],
+        "LOTUNIT": hy.split_lot_size(lotsize)[1],
+        "SIZE": size,
+        "HtY": hty_ratio or "-",
+        "HtY %": house_ratio or "-",
+        "STREET": address.split(",")[0].strip(),
+        "CITY/TOWN": address.split(",")[1].strip(),
+        "STATE": address.split(",")[2].split(" ")[1].strip(),
+        "ZIP": address.split(",")[2].split(" ")[2].strip(),
+        "LINK": f"https://www.realtor.com{link}",
+    }
+
+
+def parseSoup(URL):
+    try:
+        soup = getSoup(URL)
+        if soup is None:  # If there was an error getting the page
+            return None
+        homes = []
+        house_elements = soup.find_all("li", attrs={"data-testid": "result-card"})
+        if not house_elements:  # If there are no house elements, stop scraping
+            logging.warning(f"No house elements found at URL: {URL}")
+            return None
+        for house in house_elements:
+            home_data = getHomeData(house)
+            if home_data is not None:
+                homes.append(home_data)
+            else:
+                logging.error(f"Error when parsing house data at URL: {URL}")
+        return homes
+    except requests.exceptions.HTTPError:
+        return None
+
+
+def scrapeIt():
+    all_homes = []
+    for i in range(1, PAGES + 1):
+        URL = BASE_URL + STUB + f"pg-{i}"
+        new_homes = parseSoup(URL)
+        if new_homes is None:
+            logging.error("Stopping due to non 200 response.")
+            break
+        all_homes += new_homes
+        logging.info(f"Scraped: {URL}")
+        time.sleep(3)
+    return all_homes
+
+
+def sendIt():
+    homes = scrapeIt()
+    if homes:
+        df = pd.DataFrame(homes)
+        df.to_csv(f"scans/{LOCATION}.csv", index=False)
+        logging.info(df)
+    else:
+        logging.error("No home data scraped.")
+
+
+if __name__ == "__main__":
+    sendIt()
