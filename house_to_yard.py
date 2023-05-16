3780a6b842712bc7befd0a9eac85117ad345ac8a
nronzel <86695181+nronzel@users.noreply.github.com>
2023-05-15 20:29:13 -0400

    DRYed out the code

--- house_to_yard.py
+++ house_to_yard.py
@@ -1,24 +1,13 @@
 import re
 
 
-def parse_lot_size(text):
-    # match groups of digits possibly separated by comma or period, and any non-digit characters
+def split_lot_size(text):
     match = re.match(r"([\d.,]+)([^\d]+)", text)
     if match:
         size = float(match.group(1).replace(",", ""))
         unit = match.group(2).strip()
         if "acre" in unit:
             size *= 43560  # convert acres to square feet
-        return int(size)
-    else:
-        return None
-
-
-def split_lot_size(text):
-    match = re.match(r"([\d.,]+)([^\d]+)", text)
-    if match:
-        size = match.group(1)
-        unit = match.group(2).strip()
         return size, unit
     else:
         return None, None
